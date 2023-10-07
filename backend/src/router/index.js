1a07c8b66607eb7b499703c921e113baaa5ff274
Van Thanh <foxrainsad@gmail.com>
2023-10-07 16:04:58 +0700

    build layout front end

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -1,23 +1,73 @@
-import {createRouter, createWebHistory} from "vue-router";
-import Home from "../views/Home.vue"
-import Login from "../views/Login.vue"
+import { createRouter, createWebHistory } from "vue-router";
+import AppLayout from "../components/AppLayout.vue";
+import Login from "../views/Login.vue";
+import Dashboard from "../views/Dashboard.vue";
+import Products from "../views/Products.vue";
+import RequestPassword from "../views/RequestPassword.vue";
+import ResetPassword from "../views/ResetPassword.vue";
+import store from "../store";
 
 const routes = [
     {
-        path: '/home',
-        name: 'home',
-        component : Home
+        path: "/app",
+        name: "app",
+        component: AppLayout,
+        meta: {
+            requiresAuth: true,
+        },
+        children: [
+            {
+                path: "dashboard",
+                name: "app.dashboard",
+                component: Dashboard,
+            },
+            {
+                path: "products",
+                name: "app.products",
+                component: Products,
+            },
+        ],
     },
     {
-        path: '/login',
-        name: 'login',
-        component: Login
-    }
+        path: "/login",
+        name: "login",
+        component: Login,
+        meta: {
+            requiresGuest: true,
+        },
+    },
+    {
+        path: "/request-password",
+        name: "requestPassword",
+        component: RequestPassword,
+        meta: {
+            requiresGuest: true,
+        },
+    },
+    {
+        path: "/reset-password/:token",
+        name: "resetPassword",
+        component: ResetPassword,
+        meta: {
+            requiresGuest: true,
+        },
+    },
 ];
 
 const router = createRouter({
     history: createWebHistory(),
-    routes
-})
+    routes,
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+    console.log(to);
+    if (to.meta.requiresAuth && !store.state.user.token) {
+        next({ name: "login" });
+    } else if (to.meta.requiresGuest && store.state.user.token) {
+        next({ name: "app.dashboard" });
+    } else {
+        next();
+    }
+});
+
+export default router;
